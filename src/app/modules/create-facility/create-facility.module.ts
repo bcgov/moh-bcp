76696c3e30b9a8d826012ec9a0adf6776474d4f9
Adam Coard <arcoard+github@gmail.com>
2019-10-03 10:28:41 -0400

    Created modules

---
+++ src/app/modules/create-facility/create-facility.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CreateFacilityRoutingModule } from './create-facility-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CreateFacilityRoutingModule
+  ]
+})
+export class CreateFacilityModule { }
