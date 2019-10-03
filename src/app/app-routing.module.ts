f74fc3184183bbd0e2fdb0448a862967c6dd5a5c
Adam Coard <arcoard+github@gmail.com>
2019-10-03 10:46:56 -0400

    Register faciliy routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'register-facility',
+    loadChildren: () => import('./modules/create-facility/create-facility.module').then(m => m.CreateFacilityModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/create-facility/create-facility-routing.module.ts
+++ src/app/modules/create-facility/create-facility-routing.module.ts
@@ -1,8 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ApplicantInfoComponent } from './pages/applicant-info/applicant-info.component';
+import { FacilityInfoComponent } from './pages/facility-info/facility-info.component';
+import { ReviewComponent } from './pages/review/review.component';
+import { SubmissionComponent } from './pages/submission/submission.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'applicant-info',
+    component: ApplicantInfoComponent
+  },
+  {
+    path: 'facility-info',
+    component: FacilityInfoComponent,
+  },
+  {
+    path: 'review',
+    component: ReviewComponent
+  },
+  {
+    path: 'submission',
+    component: SubmissionComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
