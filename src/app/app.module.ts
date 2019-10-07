68b8d53b0e0723e3706372389f0a06c05140de13
Adam Coard <arcoard+github@gmail.com>
2019-10-07 08:44:10 -0700

    Added common header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedCoreModule } from 'moh-common-lib';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedCoreModule // necessary here for CommonHeader at top level
   ],
   providers: [],
   bootstrap: [AppComponent]
