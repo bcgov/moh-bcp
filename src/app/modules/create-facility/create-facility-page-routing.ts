246c3e32bb14c30b8052d6595210d6063122b30e
Adam Coard <arcoard+github@gmail.com>
2019-10-07 09:06:46 -0700

    Routing for indivdiual page - needs own file tos top ciruclar dependency with Container

---
+++ src/app/modules/create-facility/create-facility-page-routing.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ApplicantInfoComponent } from './pages/applicant-info/applicant-info.component';
+import { FacilityInfoComponent } from './pages/facility-info/facility-info.component';
+import { ReviewComponent } from './pages/review/review.component';
+import { SubmissionComponent } from './pages/submission/submission.component';
+/** The individual page routes only, does not include container */
+export const createFacilityPageRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'applicant-info',
+    component: ApplicantInfoComponent
+  },
+  {
+    path: 'facility-info',
+    component: FacilityInfoComponent,
+  },
+  {
+    path: 'review',
+    component: ReviewComponent
+  },
+  {
+    path: 'submission',
+    component: SubmissionComponent
+  },
+];
