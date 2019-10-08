4efba7dcce81e82449e70418b5ab42dff1ca52bb
Adam Coard <arcoard+github@gmail.com>
2019-10-08 11:03:00 -0700

    Update practitioner number validations as per meeting

--- src/app/modules/core-bcp/practitioner-number/validate-practitioner-number.directive.ts
+++ src/app/modules/core-bcp/practitioner-number/validate-practitioner-number.directive.ts
@@ -2,9 +2,7 @@ import { Directive } from '@angular/core';
 import { ValidatorFn, AbstractControl, ValidationErrors, Validator, NG_VALIDATORS } from '@angular/forms';
 
 export const validatePractitionerNumber: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-
-  // TODO: VERIFY THIS LOGIC!  THIS WAS A NAIVE IMPLEMENTATION, CHECK SPECS.
-  const criteria: RegExp = /^[0-9]*$/;
+  const criteria: RegExp = /^\w*$/;
 
   if (control.value) {
     const result = criteria.test(control.value);
