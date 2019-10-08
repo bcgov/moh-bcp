3ac7b9f1f531bf6f3ebce8e015ceb8ed2ca2f96c
Adam Coard <arcoard+github@gmail.com>
2019-10-08 10:24:01 -0700

    Fixed circular dependency

--- src/app/modules/create-facility/models/create-facility-form.ts
+++ src/app/modules/create-facility/models/create-facility-form.ts
@@ -1,8 +1,6 @@
 import { AbstractForm } from 'moh-common-lib';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
-import { createFacilityPageRoutes } from '../create-facility-page-routing';
-import { CREATE_FACILITY } from '../create-facility-route-constants';
 
 export class CreateFacilityForm extends AbstractForm {
   links = environment.links;
@@ -18,16 +16,6 @@ export class CreateFacilityForm extends AbstractForm {
    *  next page as ordered in routes file.
    */
   continue() {
-    if (this.form && !this.form.valid){
-      return;
-    }
-
-    const urlSegment = this.router.url.replace('/' + CREATE_FACILITY, '');
-    const index = createFacilityPageRoutes
-      .map(x => x.path)
-      .indexOf(urlSegment);
-
-    const nextUrl = createFacilityPageRoutes[index + 1].path;
-    this.navigate(`/${CREATE_FACILITY}/${nextUrl}`)
+    console.log('TODO - override this function in your page.');
   }
 }
