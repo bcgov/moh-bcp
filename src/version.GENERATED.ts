1f199005b8ad1e530eb8702811c2ad2810221d2f
Adam Coard <arcoard+github@gmail.com>
2019-10-09 10:12:38 -0700

    Add version info.

---
+++ src/version.GENERATED.ts
@@ -0,0 +1,9 @@
+// DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
+export const gitCommit = '02973c2';
+export const buildTime = '10/9/2019 at 10:09:40 AM';
+/** App version retrieved from package.json. */
+export const projectVersion = '0.0.0';
+/** Human readable message  */
+export const message = 'Canonical Version: 02973c2 - 10/9/2019 at 10:09:40 AM (v0.0.0) \n<%= repo %>commits/02973c2';
+/** If true, other values should be present. */
+export const success = true;
