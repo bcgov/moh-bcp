81c7cfc6b50e3853de1d69f461f078fdefbb4704
Adam Coard <arcoard+github@gmail.com>
2019-10-09 10:12:38 -0700

    Add version info.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderService } from './services/header.service';
+import * as version from '../version.GENERATED';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,12 @@ export class AppComponent {
   title = 'moh-bcp';
 
   constructor(private headerService: HeaderService) {
+    version.success
+      ? console.log('%c' + version.message, 'color: #036; font-size: 20px; background-color: white;')
+      : console.error(version.message);
+  }
+
+  ngOnInit() {
     this.headerService.title.subscribe(x => this.title = x);
   }
 }
