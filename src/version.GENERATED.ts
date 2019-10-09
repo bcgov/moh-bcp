a6ee6a51dce98dac84aca31f1e11386c78ab57a1
Adam Coard <arcoard+github@gmail.com>
2019-10-09 10:24:29 -0700

    Fix variable typo.

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '81c7cfc';
-export const buildTime = '10/9/2019 at 10:13:57 AM';
+export const gitCommit = 'f7fecb2';
+export const buildTime = '10/9/2019 at 10:21:35 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.0.1';
 /** Human readable message  */
-export const message = 'Canonical Version: 81c7cfc - 10/9/2019 at 10:13:57 AM (v0.0.1) \nhttps://github.com/bcgov/moh-bcp/commits/81c7cfc';
+export const message = 'Canonical Version: f7fecb2 - 10/9/2019 at 10:21:35 AM (v0.0.1) \nhttps://github.com/bcgov/moh-bcp/commits/f7fecb2';
 /** If true, other values should be present. */
 export const success = true;
