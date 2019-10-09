1f3d0f3fc8cde215ba163e2599351f455dee4fe4
Adam Coard <arcoard+github@gmail.com>
2019-10-09 09:42:36 -0700

    Fix issue with local link

--- src/app/modules/create-facility/create-facility-container/create-facility-container.component.ts
+++ src/app/modules/create-facility/create-facility-container/create-facility-container.component.ts
@@ -4,6 +4,8 @@ import { createFacilityPageRoutes } from '../create-facility-page-routing';
 import { environment } from 'src/environments/environment';
 import { CREATE_FACILITY } from '../create-facility-route-constants';
 import { HeaderService } from 'src/app/services/header.service';
+import { Routes } from '@angular/router';
+import { Route } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-create-facility-container',
@@ -19,7 +21,7 @@ export class CreateFacilityContainerComponent extends Container implements OnIni
     const pageRoutesWithoutSubmission = createFacilityPageRoutes
       .filter(x => x.path !== 'submission');
 
-    this.setProgressSteps( pageRoutesWithoutSubmission );
+    this.setProgressSteps( (pageRoutesWithoutSubmission as Route[]) );
 
     // TODO: Refactor into new  service inheritis from CheckCompleteBaseService.
     // Re-consider when building out other forms and we can assess impact.
