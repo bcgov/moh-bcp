e8918f6170d34c8b5b3425f00e1ae7a150c6be41
Faheem <faheemfactor@gmail.com>
2019-10-24 13:25:19 -0700

    Mailing Address using geoEnoder for address as like physical address. Mailing Address validation updated on question 'Is Same mailing address'. Common-radio built in messages of required showing. ISSUES: Common-Date is not setting dates as per SimpleDate. Common-Date still shows required error if value supplied from frontend. common-radio suppress buit-in message

--- src/app/modules/create-facility/pages/facility-info/facility-info.component.ts
+++ src/app/modules/create-facility/pages/facility-info/facility-info.component.ts
@@ -3,7 +3,7 @@ import { CreateFacilityForm } from '../../models/create-facility-form';
 import { Router } from '@angular/router';
 import { FormGroup, FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { cCreateFacilityValidators, validMultiFormControl } from '../../models/validators';
-import { BRITISH_COLUMBIA, CheckCompleteBaseService } from 'moh-common-lib';
+import { BRITISH_COLUMBIA, CheckCompleteBaseService, SimpleDate } from 'moh-common-lib';
 import { CreateFacilityDataService } from '../../services/create-facility-data.service';
 import { environment } from 'src/environments/environment';
 import { RandomObjects } from '../../models/i-dataform';
@@ -39,7 +39,6 @@ export class FacilityInfoComponent extends CreateFacilityForm implements OnInit 
 
   ngOnInit() {
     this.facilityForm = this.initialize();
-    this.mailingForm = this.initializeAddressForm();
   }
 
   private initialize() {
@@ -52,43 +51,123 @@ export class FacilityInfoComponent extends CreateFacilityForm implements OnInit 
       city: [this.dataService.facInfoCity, cCreateFacilityValidators.address.city],
       province: [BRITISH_COLUMBIA],
       postalCode: [this.dataService.facInfoPostalCode, cCreateFacilityValidators.address.postalCode],
-      phoneNumber: [this.dataService.facInfoPhoneNumber, cCreateFacilityValidators.facilityDetail.phoneNumber],
-      phoneExtension: [this.dataService.facInfoPhoneExtension],
+
+      // Phone number and extensions has been removed
+      // phoneNumber: [this.dataService.facInfoPhoneNumber, cCreateFacilityValidators.facilityDetail.phoneNumber],
+      // phoneExtension: [this.dataService.facInfoPhoneExtension],
+
       faxNumber: [this.dataService.facInfoFaxNumber, cCreateFacilityValidators.facilityDetail.faxNumber],
       isSameMailingAddress: [this.dataService.facInfoIsSameMailingAddress, cCreateFacilityValidators.facilityDetail.isSameMailingAddress],
       isQualifyForBCP: [this.dataService.facInfoIsQualifyForBCP, cCreateFacilityValidators.facilityDetail.isQualifyForBCP],
-      effectiveDate: [null, Validators.required]
+      
+      // effectiveDate: [this.getEffectiveDateInCommonDateFormat, cCreateFacilityValidators.facilityDetail.effectiveDate],
+      // common-date gives error if we provide date in simpledate format for initialization, changinging to null
+      effectiveDate: [null, cCreateFacilityValidators.facilityDetail.effectiveDate],
+
+      mailingAddress: [this.dataService.facInfoMailAddress, cCreateFacilityValidators.address.streetAddress],
+      mailingCity: [this.dataService.facInfoMailCity, cCreateFacilityValidators.address.city],
+      mailingProvince: [BRITISH_COLUMBIA],
+      mailingPostalCode: [this.dataService.facInfoMailPostalCode, cCreateFacilityValidators.address.postalCode],
+
     });
+
+    form.get('isSameMailingAddress').valueChanges.subscribe(
+      value => this.updateMailingValidity(value)
+    );
+
+    this.showMailingAddress = !this.dataService.facInfoIsSameMailingAddress;
     return form;
   }
 
-  private initializeAddressForm() {
-    const form = this.fb.group({
-      mailing_address: [this.dataService.facInfoMailAddress, cCreateFacilityValidators.address.streetAddress],
-      mailing_city: [this.dataService.facInfoMailCity, cCreateFacilityValidators.address.city],
-      mailing_province: [BRITISH_COLUMBIA],
-      mailing_postalCode: [this.dataService.facInfoMailPostalCode, cCreateFacilityValidators.address.postalCode],
-    });
-    return form;
+  //#region Mailing Address update data service effective date
+  // following code intentionally kept simple for maintenance 
+
+  public get getEffectiveDateInCommonDateFormat(): SimpleDate {
+
+    // issue with common-date. It does not set the common-date at all, in simpledate or initialization
+    // ref: bcp-16 #comment-24615
+    // https://github.com/bcgov/moh-common-styles/blob/master/projects/common/lib/models/simple-date.interface.ts
+    // https://github.com/bcgov/moh-common-styles/blob/master/projects/common/lib/components/date/date.component.ts
+
+    // tbr
+    const date = this.dataService.facInfoEffectiveDate? this.dataService.facInfoEffectiveDate : new Date();
+
+    const commonDateFormat = {
+      day: date.getDay(),
+      month: date.getMonth(),
+      year: date.getFullYear(),
+    }
+    return commonDateFormat;
   }
 
-  toggleMailingAddress(formGroup, status) {
-    console.log(status);
-    this.showMailingAddress = status ? false : true;
-    this.facilityForm.patchValue({ isSameMailingAddress: status });
+  updateMailingValidity(isRequired: boolean): void {
+    const address = this.facilityForm.get('mailingAddress');
+    const city = this.facilityForm.get('mailingCity');
+    const province = this.facilityForm.get('mailingProvince');
+    const postalCode = this.facilityForm.get('mailingPostalCode');
+    if (isRequired) {
+      address.setValidators(Validators.required);
+      city.setValidators(Validators.required);
+      province.setValidators(Validators.required);
+      postalCode.setValidators(Validators.required);
+    } else {
+      address.clearValidators();
+      city.clearValidators();
+      province.clearValidators();
+      postalCode.clearValidators();
+
+      address.patchValue(null);
+      city.patchValue(null);
+      province.patchValue(null);
+      postalCode.patchValue(null);
+    }
+
+    address.updateValueAndValidity();
+    city.updateValueAndValidity();
+    province.updateValueAndValidity();
+    postalCode.updateValueAndValidity();
+
+    this.showMailingAddress = !isRequired;
+    this.facilityForm.patchValue({
+      mailingAddress: null,
+      mailingCity: null,
+      mailingProvince: null,
+      mailingPostalCode: null
+    });
   }
+
+  updateDataService() {
+    // As per direction  to use dataservice for state ref:bcp-68 bcp-69 comments
+
+    const fd = this.facilityForm.value;
+    this.dataService.facInfoFacilityName = fd.facilityName;
+    this.dataService.facInfoPhysicalAddress = fd.address;
+    this.dataService.facInfoCity = fd.city;
+    this.dataService.facInfoProvince = fd.province;
+    this.dataService.facInfoPostalCode = fd.postalCode;
+    this.dataService.facInfoFaxNumber = fd.faxNumber;
+    this.dataService.facInfoIsSameMailingAddress = fd.isSameMailingAddress;
+    this.dataService.facInfoIsQualifyForBCP = fd.isQualifyForBCP;
+    this.dataService.facInfoEffectiveDate = fd.effectiveDate;
+
+    this.dataService.facInfoMailAddress = fd.mailingAddress;
+    this.dataService.facInfoMailCity = fd.mailingCity;
+    this.dataService.facInfoMailProvince = fd.mailingProvince;
+    this.dataService.facInfoMailPostalCode = fd.mailingPostalCode;
+  }
+
+  //#region 
 
   continue() {
+    this.updateDataService();
+
+
+    // todo: fix common-components issues
     this.facilityForm.markAllAsTouched();
-    this.markAllInputsTouched();
-
     // this.markAllInputsTouched();
-    //this.navigate('register-facility/review');
-  }
 
-  patchValue(formGroup) {
-    if (!environment.useDummyData) return;
-    formGroup.patchValue(RandomObjects.getFacilityInfo('test'));
+    if (this.facilityForm.valid) {
+      this.navigate('register-facility/review');
+    }
   }
-
 }
