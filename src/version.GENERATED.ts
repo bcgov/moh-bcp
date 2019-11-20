fc4e6f4c5d1dc7cc0eaaf743778d610006fbc039
Adam Coard <arcoard+github@gmail.com>
2019-11-20 13:44:25 -0500

    Fix parse errors

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'a8f5aee';
-export const buildTime = '11/19/2019 at 9:00:03 PM';
+export const gitCommit = '1bb7f70';
+export const buildTime = '11/20/2019 at 10:44:18 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.4.9';
 /** Human readable message  */
-export const message = 'Canonical Version: a8f5aee - 11/19/2019 at 9:00:03 PM (v0.4.9) \nhttps://github.com/bcgov/moh-bcp/commits/a8f5aee';
+export const message = 'Canonical Version: 1bb7f70 - 11/20/2019 at 10:44:18 AM (v0.4.9) \nhttps://github.com/bcgov/moh-bcp/commits/1bb7f70';
 /** If true, other values should be present. */
 export const success = true;
