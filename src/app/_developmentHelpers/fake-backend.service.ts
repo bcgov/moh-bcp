9af4a510ad3e36e563e73ae926bf00770f760ae3
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-11-21 13:04:28 -0500

    Checkpoint - added fake-backend all returnCode 0

---
+++ src/app/_developmentHelpers/fake-backend.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest } from '@angular/common/http';
+import { APIBase, ReturnCodes } from '../modules/create-facility/models/create-facility-api-model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FakeBackendService {
+
+  constructor() {}
+
+  validatePractitioner( request: HttpRequest<any> ): any {
+
+    // TODO: Create different return values base on data sent in request
+    const obj = {
+      returnCode: ReturnCodes.SUCCESS,
+      message: 'MATCH'
+    };
+
+    return Object.assign(this._baseResponse( request ), obj);
+  }
+
+  validateFacility(request: HttpRequest<any> ): any {
+
+    // TODO: Create different return values base on data sent in request
+    const obj = {
+      returnCode: ReturnCodes.SUCCESS,
+      message: 'MATCH'
+    };
+
+    return Object.assign(this._baseResponse( request ), obj);
+  }
+
+  createFacility(request: HttpRequest<any> ): any {
+    const referenceNo = 'BCP' + String( Math.round( Math.random() * 99999999 ) );
+
+    // TODO: Create different return values base on data sent in request
+    const obj = {
+      returnCode: ReturnCodes.SUCCESS,
+      referenceNumber: referenceNo,
+      facilityNumber: 'F' + String( Math.round( Math.random() * 999999 ) )
+    };
+
+    return Object.assign(this._baseResponse( request ), obj);
+  }
+
+
+  private _baseResponse( request: HttpRequest<any> ): APIBase {
+    return {
+      requestUUID: request.body.requestUUID,
+      applicationUUID: request.body.applicationUUID,
+    };
+  }
+
+}
