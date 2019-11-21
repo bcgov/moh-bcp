e76accb9d5c1d094d19e8643b5fbb86cc571f1bf
cyril bravo <cyril.bravo@vs-dev015.maximusbchealth.local>
2019-11-21 15:43:13 -0500

    Modified happy path

--- e2e/register-facility/src/bcp-reg-facility-happy-path.e2e-spec.ts
+++ e2e/register-facility/src/bcp-reg-facility-happy-path.e2e-spec.ts
@@ -36,4 +36,16 @@ describe('BCP Register Facility - End to End Test (Happy Path)', () => {
         expect(browser.getCurrentUrl()).toContain(SUBMISSION_PAGE_URL, 'should navigate to the Submission Page');
     }, 100000);
 
+    // it('02. should navigate from Home Page to Submission Page (end-to-end) when all required fields are filled out', () => {
+    //     homePage.navigateTo();
+    //     homePage.fillPage();
+    //     expect(browser.getCurrentUrl()).toContain(ADMIN_PAGE_URL, 'should navigate to the Facility Admin Page');
+    //     adminPage.fillPage();
+    //     expect(browser.getCurrentUrl()).toContain(INFO_PAGE_URL, 'should navigate to the Facility Info Page');
+    //     infoPage.fillPage();
+    //     expect(browser.getCurrentUrl()).toContain(REVIEW_PAGE_URL, 'should navigate to the Review Page');
+    //     reviewPage.fillPage();
+    //     expect(browser.getCurrentUrl()).toContain(SUBMISSION_PAGE_URL, 'should navigate to the Submission Page');
+    // }, 100000);
+
 });
