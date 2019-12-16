8bacb0e5917c9adff9e45992c483ba257be41d4a
cyrilbravo <cyril.bravo@maximusbc.ca>
2019-12-16 13:07:44 -0500

    Updated Node.js version to 10.9.0

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'd6532f5';
-export const buildTime = '12/10/2019 at 3:19:20 PM';
+export const gitCommit = '983e7b4';
+export const buildTime = '12/16/2019 at 9:56:08 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.9.0';
 /** Human readable message  */
-export const message = 'Canonical Version: d6532f5 - 12/10/2019 at 3:19:20 PM (v0.9.0) \nhttps://github.com/bcgov/moh-bcp/commits/d6532f5';
+export const message = 'Canonical Version: 983e7b4 - 12/16/2019 at 9:56:08 AM (v0.9.0) \nhttps://github.com/bcgov/moh-bcp/commits/983e7b4';
 /** If true, other values should be present. */
 export const success = true;
