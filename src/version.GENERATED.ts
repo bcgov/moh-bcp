8214c87b76a69d2afc04184bf03ffb7eb0434cb9
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2019-12-17 13:28:33 -0500

    Removed file not used

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '656a115';
-export const buildTime = '12/16/2019 at 7:07:52 PM';
+export const gitCommit = 'ebfddad';
+export const buildTime = '12/17/2019 at 10:24:53 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.9.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 656a115 - 12/16/2019 at 7:07:52 PM (v0.9.0) \nhttps://github.com/bcgov/moh-bcp/commits/656a115';
+export const message = 'Canonical Version: ebfddad - 12/17/2019 at 10:24:53 AM (v0.9.0) \nhttps://github.com/bcgov/moh-bcp/commits/ebfddad';
 /** If true, other values should be present. */
 export const success = true;
