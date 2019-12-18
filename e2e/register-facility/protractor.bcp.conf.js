997befe9ee3fe8805989dd4f14697cb2bd640b4d
cyrilbravo <cyril.bravo@maximusbc.ca>
2019-12-18 17:09:14 -0500

    Modified e2e yaml

--- e2e/register-facility/protractor.bcp.conf.js
+++ e2e/register-facility/protractor.bcp.conf.js
@@ -14,9 +14,17 @@ exports.config = {
   specs: [
     './src/bcp-reg-facility-happy-path.e2e-spec.ts'
   ],
-  capabilities: {
-    'browserName': 'firefox',
-  },
+  multiCapabilities: [
+    {
+      browserName: 'firefox',
+      firefoxOptions: {
+        args: ['--headless']
+      },
+      'moz:firefoxOptions': {
+        args: [ '--headless' ]
+      }
+    }
+  ],
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
