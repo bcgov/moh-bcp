bd0a46f33fe359e36f53aab3422e87eaa98c2dd6
cyrilbravo <cyril.bravo@maximusbc.ca>
2020-01-06 11:33:24 -0500

    Change minor details in e2e

--- e2e/register-facility/src/bcp-reg-facility-happy-path.e2e-spec.ts
+++ e2e/register-facility/src/bcp-reg-facility-happy-path.e2e-spec.ts
@@ -15,9 +15,9 @@ export function regFacilityTest(index: number) {
     it('should navigate from Home Page to Submission Page (end-to-end) when all required fields are filled out', () => {
         homePage.navigateTo();
         homePage.fillPage(index);
-        expect(browser.getCurrentUrl()).toContain(CREATE_FACILITY_PAGES.FACILITY_ADMIN.fullpath, 'should navigate to the Facility Admin Page');
+        expect(browser.getCurrentUrl()).toContain(CREATE_FACILITY_PAGES.FACILITY_ADMIN.fullpath, 'navigate to the Facility Admin Page');
         adminPage.fillPage(index);
-        expect(browser.getCurrentUrl()).toContain(CREATE_FACILITY_PAGES.FACILITY_INFO.fullpath, 'should navigate to the Facility Info Page');
+        expect(browser.getCurrentUrl()).toContain(CREATE_FACILITY_PAGES.FACILITY_INFO.fullpath, 'navigate to the Facility Info Page');
         infoPage.fillPage(index);
         expect(browser.getCurrentUrl()).toContain(CREATE_FACILITY_PAGES.REVIEW.fullpath, 'should navigate to the Review Page');
         reviewPage.fillPage(index);
