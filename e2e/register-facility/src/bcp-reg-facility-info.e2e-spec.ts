2bb996ba1b6dae4284fde63cad89e4af2d54a47b
cyrilbravo <cyril.bravo@maximusbc.ca>
2020-01-13 14:09:13 -0500

    Add test cases for validation

--- e2e/register-facility/src/bcp-reg-facility-info.e2e-spec.ts
+++ e2e/register-facility/src/bcp-reg-facility-info.e2e-spec.ts
@@ -5,18 +5,27 @@ import { CREATE_FACILITY_PAGES } from '../../../src/app/modules/create-facility/
 describe('BCP Register Facility - Info Page (Unit Test)', () => {
 
     let infoPage: BCPInfoPage;
-
-    const DEFAULT_DATA = 0;
-    const MAX_VAL_DATA = 1;
+    let index = 0;
 
     beforeEach(() => {
         infoPage = new BCPInfoPage();
     });
 
+    afterEach(() => {
+        index++;
+    });
+
     // VALIDATION TESTS
-    it('01. should be able to continue if all the required fields are filled out and valid', () => {
+    it('01. should be able to continue if the facility name is VALID', () => {
         infoPage.navigateTo();
-        infoPage.fillPage(DEFAULT_DATA);
+        infoPage.fillPage(index);
+        infoPage.clickContinue();
+        expect(browser.getCurrentUrl()).toContain(CREATE_FACILITY_PAGES.REVIEW.fullpath, 'should continue to the next page');
+    });
+
+    it('02. should be able to continue even if the facility name is INVALID', () => {
+        infoPage.navigateTo();
+        infoPage.fillPage(index);
         infoPage.clickContinue();
         expect(browser.getCurrentUrl()).toContain(CREATE_FACILITY_PAGES.REVIEW.fullpath, 'should continue to the next page');
     });
