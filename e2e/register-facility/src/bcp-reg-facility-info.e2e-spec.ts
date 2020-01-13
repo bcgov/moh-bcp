5cc33bf3b4f300339e8397da4864ce00a1089291
cyrilbravo <cyril.bravo@maximusbc.ca>
2020-01-13 12:02:35 -0500

    Modified for validation e2e

--- e2e/register-facility/src/bcp-reg-facility-info.e2e-spec.ts
+++ e2e/register-facility/src/bcp-reg-facility-info.e2e-spec.ts
@@ -13,23 +13,11 @@ describe('BCP Register Facility - Info Page (Unit Test)', () => {
         infoPage = new BCPInfoPage();
     });
 
-    it('01. should not be able to continue if the required fields are not filled out', () => {
-        infoPage.navigateTo();
-        infoPage.clickContinue();
-        expect(browser.getCurrentUrl()).toContain(CREATE_FACILITY_PAGES.FACILITY_INFO.fullpath, 'should stay on the same page');
-    });
-
-    it('02. should be able to input values in their maximum capacity', () => {
-        infoPage.navigateTo();
-        infoPage.fillPage(MAX_VAL_DATA);
-        infoPage.checkInfoInputValues(MAX_VAL_DATA);
-    });
-
-    it('03. should be able to continue if all the required fields are filled out and valid', () => {
+    // VALIDATION TESTS
+    it('01. should be able to continue if all the required fields are filled out and valid', () => {
         infoPage.navigateTo();
         infoPage.fillPage(DEFAULT_DATA);
         infoPage.clickContinue();
         expect(browser.getCurrentUrl()).toContain(CREATE_FACILITY_PAGES.REVIEW.fullpath, 'should continue to the next page');
     });
-
 });
