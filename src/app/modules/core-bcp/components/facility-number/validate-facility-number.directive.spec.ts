37cfa072d408ac474161a78bc6aa600e7fb24908
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2020-01-14 18:23:13 -0500

    work in progress

--- src/app/modules/core-bcp/components/facility-number/validate-facility-number.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValidateFacilityNumberDirective } from './validate-facility-number.directive';
-
-describe('ValidateFacilityNumberDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ValidateFacilityNumberDirective();
-    expect(directive).toBeTruthy();
-  });
-});
