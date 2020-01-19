ab94c72acf344b63d5af5ca983312d2c0b062212
Harry Maynard <harry.maynard@maximusbc.ca>
2020-01-19 15:51:09 -0500

    Clean-up.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   bypassModal: false,
   bypassGuards: true,
-  useDummyData: true, // For testing against real back-end
+  useDummyData: false, // For testing against real back-end
   useMockBackend: false,
   useMockBackendData: false,
   api: {
