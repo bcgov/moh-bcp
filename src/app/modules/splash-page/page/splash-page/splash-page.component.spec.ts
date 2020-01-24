9819ee1409afdaf80327c815bc169022197b0ee5
Harry Maynard <harry.maynard@maximusbc.ca>
2020-01-24 12:16:39 -0500

    BCP-235: Fixed unit test.

--- src/app/modules/splash-page/page/splash-page/splash-page.component.spec.ts
+++ src/app/modules/splash-page/page/splash-page/splash-page.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SplashPageComponent } from './splash-page.component';
 import {RouterTestingModule} from '@angular/router/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import { SharedCoreModule } from 'moh-common-lib';
 
 describe('SplashPageComponent', () => {
   let component: SplashPageComponent;
@@ -15,7 +16,8 @@ describe('SplashPageComponent', () => {
       ],
       imports: [
         HttpClientTestingModule,
-        RouterTestingModule
+        RouterTestingModule,
+        SharedCoreModule,
       ]
     })
     .compileComponents();
