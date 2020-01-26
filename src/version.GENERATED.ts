cc55660726ca94520ca0b1b5e6e58a4677f50ace
kristin reed <kristin.reed@vs-dev011.maximusbchealth.local>
2020-01-26 00:29:08 -0500

    udpate libarary to v3.0.5 to fix email validation

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '5ab5c5c';
-export const buildTime = '1/25/2020 at 3:19:48 PM';
+export const gitCommit = 'c523807';
+export const buildTime = '1/25/2020 at 6:49:40 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.11.6';
 /** Human readable message  */
-export const message = 'Canonical Version: 5ab5c5c - 1/25/2020 at 3:19:48 PM (v0.11.6) \nhttps://github.com/bcgov/moh-bcp/commits/5ab5c5c';
+export const message = 'Canonical Version: c523807 - 1/25/2020 at 6:49:40 PM (v0.11.6) \nhttps://github.com/bcgov/moh-bcp/commits/c523807';
 /** If true, other values should be present. */
 export const success = true;
