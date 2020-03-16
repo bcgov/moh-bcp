5b32facacf436abcc8a6a98fc673820362de5a2d
Harry Maynard <harry.maynard@maximusbc.ca>
2020-03-16 15:35:51 -0400

    BCP-342: Fixed postal code validation in change facility.

--- src/app/modules/core-bcp/models/validators.ts
+++ src/app/modules/core-bcp/models/validators.ts
@@ -1,4 +1,5 @@
 import { FormGroup } from '@angular/forms';
+import { commonValidatePostalcode } from 'moh-common-lib';
 
 export function validMultiFormControl(fg: FormGroup, name: string) {
   if (fg.controls[name].pristine && fg.controls[name].untouched ) {
@@ -6,3 +7,5 @@ export function validMultiFormControl(fg: FormGroup, name: string) {
   }
   return fg.controls[name].invalid;
 }
+
+export const validatePostalCode = commonValidatePostalcode( true, true ); // Must be BC Postal Code.
